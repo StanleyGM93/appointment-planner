8b61f64a3d164b93810c7c782e985dbf9a660eda
Stanley Mair <stanleygmair@gmail.com>
2022-09-16 05:05:55 +1200

    Added a method to iterate over an onject to display values in p elements

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-export const Tile = () => {
-  return (
-    <div className="tile-container">
-      
-    </div>
-  );
+export const Tile = (obj) => {
+  const keys = Object.keys(obj);
+  const mappingKeys = keys.map((key) => {
+    if (key[0]) {
+      return <p className="tile-title">{obj.key}</p>;
+    } else {
+      return <p className="tile">{obj.key}</p>;
+    }
+  });
+
+  return <div className="tile-container">{mappingKeys}</div>;
 };
