a5c627e6283cc9772a7a2ca5c7e1746dacde67ce
Stanley Mair <stanleygmair@gmail.com>
2022-09-20 04:53:07 +1200

    Filled in the return statement with a select that has options given via a map method

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,10 +1,17 @@
 import React from "react";
 
 export const ContactPicker = ({ contactsArray, onChange }) => {
-  const contactNameGenerator = (contactsArray) => {};
+  const contactNameGenerator = contactsArray.map((contact, index) => {
+    return (
+      <option key={index} value={contact.name}>
+        {contact.name}
+      </option>
+    );
+  });
   return (
-    <select>
-      <option default></option>
+    // Need to fix the callback function as onChange isn't correct.
+    <select onChange={onChange}>
+      <option value="" selected="selected"></option>
       {contactNameGenerator}
     </select>
   );
